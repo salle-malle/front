c365661d0c59ba12420ac6628a8dfe27bb066757
임채연 <dlacodus0407@gmail.com>
2025-07-15 13:33:02 +0900

    refactor : 버튼 디테일 수정

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -12,13 +12,14 @@ export default function SignupLandingPage() {
   return (
     <div className="flex flex-col h-screen bg-white">
       <TopNavigation showBackButton={true} title="" />
-
-      <div className="flex flex-col justify-between flex-1 px-10 py-8">
+      <div className="flex flex-col justify-between flex-1 px-12 py-8">
         <div>
-          <p className="text-lg text-black mt-10 mb-[75px]" style={{ fontWeight: 500 }}>
+          <p
+            className="text-lg text-black mt-10 mb-[75px]"
+            style={{ fontWeight: 500 }}
+          >
             이름을 알려주세요
           </p>
-
           <label
             style={{ color: focus ? "#2978EE" : "#848A92" }}
             className="block text-sm font-medium mt-10 mb-1"
@@ -30,29 +31,15 @@ export default function SignupLandingPage() {
             onChange={(e) => setName(e.target.value)}
             onFocus={() => setFocus(true)}
             onBlur={() => setFocus(false)}
-            className="
-              w-full
-              h-10
-              pr-3
-              pl-0
-              bg-white
-              border-0 border-b-2
-              border-gray-300
-              rounded-none
-              focus:outline-none
-              focus:ring-0
-              focus:shadow-none
-              focus:border-[#2978EE]
-              focus-visible:ring-0
-              focus-visible:outline-none
-              text-base
-            "
+            className="w-full h-10 pr-3 pl-0 bg-white border-0 border-b-2 border-gray-300 rounded-none focus:outline-none focus:ring-0 focus:shadow-none focus:border-[#2978EE] focus-visible:ring-0 focus-visible:outline-none text-base"
             inputMode="text"
             enterKeyHint="done"
             style={{ scrollMarginTop: 100 }}
           />
         </div>
-        <Button className="w-full mx-auto h-13 bg-blue-500 hover:bg-blue-600 text-white rounded-xl mt-10 text-lg">
+      </div>
+      <div className="mb-9 flex justify-center">
+        <Button className="w-[90%] h-[40px] bg-blue-500 hover:bg-blue-600 text-white rounded-sm mt-10 text-sm shadow-lg hover:shadow-lg">
           다음
         </Button>
       </div>
