8ab1027a37a477e0459bc67a7c59aa58283609c6
임채연 <dlacodus0407@gmail.com>
2025-07-15 16:38:12 +0900

    Feat : 휴대폰 본인인증 UI 구현

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,14 +4,17 @@ import { useState } from "react";
 import { Input } from "@/src/components/ui/input";
 import { Button } from "@/src/components/ui/button";
 import { TopNavigation } from "@/src/components/top-navigation";
+import { useRouter } from "next/navigation";
 
 export default function SignupLandingPage() {
+  const router = useRouter();
   const [name, setName] = useState("");
   const [focus, setFocus] = useState(false);
 
   return (
     <div className="flex flex-col h-screen bg-white">
       <TopNavigation showBackButton={true} title="" />
+
       <div className="flex flex-col justify-between flex-1 px-12 py-8">
         <div>
           <p
@@ -38,8 +41,14 @@ export default function SignupLandingPage() {
           />
         </div>
       </div>
+
       <div className="mb-9 flex justify-center">
-        <Button className="w-[90%] h-[40px] bg-blue-500 hover:bg-blue-600 text-white rounded-sm mt-10 text-sm shadow-lg hover:shadow-lg">
+        <Button
+          onClick={() => {
+            router.push("/signup/phoneNumber");
+          }}
+          className="w-[90%] h-[40px] bg-blue-500 hover:bg-blue-600 text-white rounded-sm mt-10 text-sm shadow-lg hover:shadow-lg"
+        >
           다음
         </Button>
       </div>
